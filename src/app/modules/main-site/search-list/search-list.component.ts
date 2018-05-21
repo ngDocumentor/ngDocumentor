170b9cf6cc61d547353b900cd097609804c719e4
Ganesh B <ganeshsurfs@gmail.com>
2018-05-21 16:52:40 +0530

    Search list component structure ready

--- src/app/modules/main-site/search-list/search-list.component.ts
+++ src/app/modules/main-site/search-list/search-list.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchListComponent implements OnInit {
 
+  searchlist: any[] = [1, 2, 3, 4, 5];
   constructor() { }
 
   ngOnInit() {
