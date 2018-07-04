4ba59e6a8a6f917d9554bc84f87f5519a8e04777
Ganesh Bhat <ganeshsurfs@gmail.com>
2018-07-04 08:59:04 +0530

    1 - New cleaner router, can now handle bookmarks(TODO). 2 - New 2 landing page options a. depective page option (with two options slider or highlighter text) and b. text page option using normal home.md file. 3 - Settings file merged to one to avoid priority related complication and related error handling. 4 - Search option added with a. gross key count, b. ignoring language fillers and allowance of merging external fillers, c. keyword based search option structure added to avoid complete documents search for huge documentation (TODO - incomplete), d. (TODO: phrase search addition, dynamic filler detection using avg/mean{check} occurance of keys & document size & n value). 5 - Minor change in css. Seperating the landing page css for modification. 6. Added landing page text. 6 - (TODO - Add Tests for router). 7 - Rectifying minor css issues. 8- Adding minor documentation changes

--- src/app/modules/main-site/rendermd/rendermd.component.ts
+++ src/app/modules/main-site/rendermd/rendermd.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { MarkdownService } from 'ngx-markdown';
 import { HttpService } from '../../../commons/services/http/http.service';
 import { WorkerService } from '../../../commons/services/worker/worker.service';
-import { SearchResult } from '../../../commons/interfaces/search/search';
 
 @Component({
   selector: 'app-rendermd',
